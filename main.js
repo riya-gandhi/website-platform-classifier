ab89516b7e5cd2b121549206569dd0423154949d
Riya Gandhi <riya090401@gmail.com>
2022-07-18 20:02:31 +0530

    initialise script

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// node main.js --url=https://builtwith.com/ --input=input.csv
+
+// npm init -y
+// npm install minimist
+// npm install puppeteer
+
 let minimist = require("minimist");
 let puppeteer = require("puppeteer");
 let fs = require("fs");
+
+let args = minimist(process.argv);
+
+//take input from csv
+
+async function run() {
+  // start the browser
+  let browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+    defaultViewport: null,
+  });
+
+  // get the tabs (there is only one tab)
+  let pages = await browser.pages();
+  let page = pages[0];
+
+  // open the url
+  await page.goto(args.url);
+
+}
+run();
