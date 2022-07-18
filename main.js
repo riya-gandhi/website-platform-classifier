f9b360c5b5d5c0198dde13d79346b262ab3c349e
Riya Gandhi <riya090401@gmail.com>
2022-07-18 21:01:25 +0530

    get browser

--- main.js
+++ main.js
@@ -10,16 +10,25 @@ let fs = require("fs");
 let xl = require('excel4node');
 
 let args = minimist(process.argv);
+// start the browser
+let browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+    defaultViewport: null,
+});
+
+// get the tabs (there is only one tab)
+let pages = await browser.pages();
+let page = pages[0];
 
 async function run() {
-    //get number of nonempty rows of first col as 
-    for(let i=0;i<7;i++){
-        // get classifier
-        await getClassifier();
-        // append classifier to input.csv
-        await appendClassifier();
-    }
-
+  //get number of nonempty rows of first col as
+  for (let i = 0; i < 7; i++) {
+    // get classifier
+    await getClassifier();
+    // append classifier to input.csv
+    await appendClassifier();
+  }
 }
 run();
 
